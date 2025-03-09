4a6e7b64fe0c1dc9210ac784376c210dff6a8deb
Deepika Gandla <155216833+deepikagandla7456@users.noreply.github.com>
2025-03-09 18:52:51 +0530

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About() {
+  return (
+    <section className="container">
+      <h2>About Me</h2>
+      <p>
+        I am a third-year B.Tech Computer Science (Data Science) student with hands-on experience in Python, Django,
+        and React. I have worked on various projects like a quiz app, fake news detection system, and more.
+      </p>
+    </section>
+  );
+}
+
+export default About;
