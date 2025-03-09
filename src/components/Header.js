be4957e5ec3874f4001bb17349bab912f423a279
Deepika Gandla <155216833+deepikagandla7456@users.noreply.github.com>
2025-03-09 18:51:33 +0530

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header style={{ backgroundColor: "#4a90e2", padding: "10px" }}>
+      <nav className="container">
+        <h1 style={{ color: "white", fontSize: "24px" }}>Deepika Gandla</h1>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
