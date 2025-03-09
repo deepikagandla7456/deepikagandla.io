0b7f86cb2ce2c36c3f8158799f227edf49d1c247
Deepika Gandla <155216833+deepikagandla7456@users.noreply.github.com>
2025-03-09 18:52:15 +0530

    Create Hero.js

---
+++ src/components/Hero.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <section style={{ textAlign: "center", padding: "50px", backgroundColor: "#f5a623" }}>
+      <h1 style={{ fontSize: "48px", color: "white" }}>Hi, I'm Deepika Gandla</h1>
+      <p style={{ fontSize: "20px", color: "white" }}>
+        Enthusiastic B.Tech Computer Science Student | Full Stack Developer | Data Science Enthusiast
+      </p>
+    </section>
+  );
+}
+
+export default Hero;
